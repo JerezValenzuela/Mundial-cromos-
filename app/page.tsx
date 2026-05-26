54c68c17ba89d31ddf35d2f1a5f49780b52575c0
JerezValenzuela <samujerezv@gmail.com>
2026-05-25 22:50:10 -0500

    fix: force dynamic to avoid supabase prerender error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useStickers } from '@/hooks/useStickers'
 import { TeamCard } from '@/components/TeamCard'
 import { Header } from '@/components/Header'
